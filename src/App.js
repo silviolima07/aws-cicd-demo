06a6db60e41db97e1e16305a81bbb11b5a53fd28
Silvio <silviolima07@gmail.com>
2021-04-24 22:05:18 -0300

    Changed <br></br>

--- src/App.js
+++ src/App.js
@@ -6,14 +6,14 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" width="300" height="300"/>
+        <img src={logo} className="App-logo" alt="logo" width="320" height="320"/>
         <p>
            <center><strong>AWS - Continous Integration / Continous Delivery</strong></center>
 		<br></br>
 		Example with a React application<br></br>
-		Source -&nbspgithub<br></br>
-		CodeBuild -&nbspwhen code is pushed by developer, code is built and application zipped.<br></br>
-		CodeDeploy -&nbspapplication is unzipped in the bucket created to host a static site<br></br>
+		Source - github<br></br>
+		CodeBuild - when code is pushed by developer, code is built and application zipped.<br></br>
+		CodeDeploy - application is unzipped in the bucket created to host a static site<br></br>
         </p>
         <a
           className="App-link"
