043add084df7f36543779d800e58e65d9630c854
Silvio <silviolima07@gmail.com>
2021-04-24 21:16:15 -0300

    Changed message on app.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          AWS - Pipeline is working!!!
+           <center><strong>AWS - Continous Integration / Continous Delivery</strong>
+		<br>
+		<strong>React application</strong><br>
+		<strong>Source:</strong>GitHub<br>
+		<strong>CodeBuild:</strong><br>
+		After executing push by git, code is compiled and application is zipped
+		<br><strong>CodeDeploy:</strong><br>
+		Application is unzipped in the bucket created to host a static site
+		</center>
         </p>
         <a
           className="App-link"
@@ -16,7 +24,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         > 
-        <img src={cicd}/>
+        <img src={cicd} alt="CI/CD"/>
         </a>
       </header>
     </div>
